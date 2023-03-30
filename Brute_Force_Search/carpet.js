c5cb23b07afdb18225efe015d42b9d09c83ab51b
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-03-30 21:09:51 +0900

    Update carpet.js
    comment about my issue

--- Brute_Force_Search/carpet.js
+++ Brute_Force_Search/carpet.js
@@ -1,5 +1,12 @@
 /*https://school.programmers.co.kr/questions/27737*/
 
+/*
+테스트 케이스가 (18,6) 이면 답은 (8,3)이어야 함
+왜냐면 총합이 24일 때 답을 (6,4)라고 하면
+테스트 케이스 (16, 8) 또한 답이 (6,4)이기 때문.
+개인적으론 잘 이해되지 않는 측면.
+*/
+
 function solution(brown, yellow) {
   let divisors = getDivisors(brown+yellow);
   if(divisors.length % 2 == 0) {
