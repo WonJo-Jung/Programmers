0e2908b0d3b3634c7561b7d79e6323ad94f7a582
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-04-06 09:57:16 +0900

    add reference document

--- Heap/disc_controller.js
+++ Heap/disc_controller.js
@@ -1,4 +1,5 @@
 /*https://velog.io/@davidko/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%94%94%EC%8A%A4%ED%81%AC-%EC%BB%A8%ED%8A%B8%EB%A1%A4%EB%9F%ACjava*/
+/*https://jun-choi-4928.medium.com/javascript%EB%A1%9C-heap-priority-queue-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-8bc13bf095d9*/
 
 /*
 function solution(jobs) {
