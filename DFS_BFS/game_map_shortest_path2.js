d7cc0329583d154fd050b19fc09995e1d88c5f07
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-04-18 11:50:20 +0900

    Update game_map_shortest_path2.js

--- DFS_BFS/game_map_shortest_path2.js
+++ DFS_BFS/game_map_shortest_path2.js
@@ -7,29 +7,31 @@ class Position {
 }
 
 function solution(maps) {
-  let queue = [];
-  queue.push(new Position(0,0,1));
-  return game_map(maps, queue);
+  let stack = [];
+  stack.push(new Position(0,0,1));
+  return game_map(maps, stack);
 }
 
-function game_map(maps, queue) {
+function game_map(maps, stack) {
   let answer = -1;
   const row = maps.length;
   const col = maps[0].length;
-  while(queue.length > 0) {
-    let pos = queue.shift();
-    maps[pos.y][pos.x] = 0;
+  while(stack.length > 0) {
+    let pos = stack.pop();
+    if(!maps[pos.y][pos.x]) break;
     if(pos.y == row-1 && pos.x == col-1) {
-      return pos.count;
+      answer = pos.count;
+      break;
     }
-    // down
-    if(pos.y+1 < row && maps[pos.y+1][pos.x]) queue.push(new Position(pos.y+1, pos.x, pos.count+1));
-    // right
-    if(pos.x+1 < col && maps[pos.y][pos.x+1]) queue.push(new Position(pos.y, pos.x+1, pos.count+1));
-    // up
-    if(pos.y-1 > -1 && maps[pos.y-1][pos.x]) queue.push(new Position(pos.y-1, pos.x, pos.count+1));
-    // left
-    if(pos.x-1 > -1 && maps[pos.y][pos.x-1]) queue.push(new Position(pos.y, pos.x-1, pos.count+1));
+    maps[pos.y][pos.x] = 0;
+    if(pos.x-1 > -1 && maps[pos.y][pos.x-1]) // left
+      stack.push(new Position(pos.y, pos.x-1, pos.count+1));
+    if(pos.y-1 > -1 && maps[pos.y-1][pos.x]) // up
+      stack.push(new Position(pos.y-1, pos.x, pos.count+1));
+    if(pos.y+1 < row && maps[pos.y+1][pos.x]) // down
+      stack.push(new Position(pos.y+1, pos.x, pos.count+1));
+    if(pos.x+1 < col && maps[pos.y][pos.x+1]) // right
+      stack.push(new Position(pos.y, pos.x+1, pos.count+1));
   }
   return answer;
 }
